57aec361541b658e3b267c58f809f673ca1be4bb
Dennis Hofirek <dennis.hofirek@outlook.de>
2023-07-09 15:31:28 +0200

    Fixed layout for Single Choice Quiz

--- frontend/src/app/views/dashboard/Page_MC.jsx
+++ frontend/src/app/views/dashboard/Page_MC.jsx
@@ -1,4 +1,4 @@
-import { Grid, styled } from '@mui/material';
+import { Grid, styled, Box, Card } from '@mui/material';
 import { Fragment } from 'react';
 import MultipleChoice from './shared/MultipleChoice';
 
@@ -8,11 +8,23 @@ const ContentBox = styled('div')(({ theme }) => ({
 }));
 
 const Title = styled('span')(() => ({
-  fontSize: '2.5rem',
+  fontSize: '1rem',
   fontWeight: '500',
-  marginRight: '.5rem',
   textTransform: 'capitalize',
-  width: '100%'
+}));
+
+const CardHeader = styled(Box)(() => ({
+  display: 'flex',
+  paddingLeft: '24px',
+  paddingRight: '24px',
+  marginBottom: '12px',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+}));
+
+const SubTitle = styled('span')(({ theme }) => ({
+  fontSize: '0.875rem',
+  color: theme.palette.text.secondary,
 }));
 
 const Analytics = () => {
@@ -22,7 +34,14 @@ const Analytics = () => {
       <ContentBox className="analytics">
         <Grid container spacing={3}>
           <Grid item lg={12} md={12} sm={12} xs={12}>
-            <Title>Single Choice Movie Quiz</Title>
+            <Card elevation={3} sx={{ pt: '20px', mb: 3 }}>
+            <CardHeader>
+              <Title>Single Choice Movie Quiz</Title>
+              </CardHeader>
+              <CardHeader>
+                <SubTitle>Select one out of five possible answers. To submit your answer, press the Button on the right.</SubTitle>
+              </CardHeader>
+            </Card>
             <MultipleChoice />
           </Grid>
         </Grid>
